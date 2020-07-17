03580ae937117a7e2181cd1f8cd5ee2949b2b22a
Dennis7456 <denniskiplangat.dk@gmail.com>
2020-07-17 16:20:14 +0300

    Rendering Error messages with Vanilla Js

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,19 @@
+import random
+from django.conf import settings
 from django.db import models
+
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-id']
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 200)
+        }
