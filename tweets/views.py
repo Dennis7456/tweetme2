5ef3c67c96c7bdb9293c9575d5d896d06d41fa80
Dennis7456 <denniskiplangat.dk@gmail.com>
2020-07-21 16:54:38 +0300

    Permissions and Authentication Classes Decorators for DRF APIs

--- tweets/views.py
+++ tweets/views.py
@@ -4,7 +4,10 @@ from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
-from rest_framework.decorators import api_view
+
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .forms import TweetForm
 from .models import Tweet
@@ -17,13 +20,15 @@ def home_view(request, *args, **kwargs):
 
 
 @api_view(['POST']) # http method the client == POST
+# @authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data=request.POST or None)
     if serializer.is_valid(raise_exception=True):
         serializer.save(user=request.user)
         return Response(serializer.data, status=201)
     return Response({}, status=400)
-    
+
 @api_view(['GET'])
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     qs = Tweet.objects.filter(id = tweet_id)
@@ -73,20 +78,20 @@ def tweet_list_view(request, *args, **kwargs):
 #     }
 #     return JsonResponse(data)
 
-def tweet_detail_view_pure_django(request, tweet_id, *args, **kwargs):
-    """
-    REST API VIEW
-    Consume by JavaScript or Swift/Java/iOS/Android
-    return json data
-    """
-    data = {
-        "id": tweet_id,
-    }
-    status = 200
-    try:
-        obj = Tweet.objects.get(id=tweet_id)
-        data['content'] = obj.content
-    except:
-        data['message'] = "Not found"
-        status = 404
-    return JsonResponse(data, status=status) #json.dump content_type='application/json'
+# def tweet_detail_view_pure_django(request, tweet_id, *args, **kwargs):
+#     """
+#     REST API VIEW
+#     Consume by JavaScript or Swift/Java/iOS/Android
+#     return json data
+#     """
+#     data = {
+#         "id": tweet_id,
+#     }
+#     status = 200
+#     try:
+#         obj = Tweet.objects.get(id=tweet_id)
+#         data['content'] = obj.content
+#     except:
+#         data['message'] = "Not found"
+#         status = 404
+#     return JsonResponse(data, status=status) #json.dump content_type='application/json'
