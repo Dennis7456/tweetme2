b2183ab236dcf7aef134fe340f42ba9268b74b33
Dennis7456 <denniskiplangat.dk@gmail.com>
2020-07-21 17:43:22 +0300

    Deleting a single tweet

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -126,13 +126,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-REST_FRAMEWORK = {
 
+DEFAULT_RENDERER_CLASSES = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
+if DEBUG:
+    DEFAULT_RENDERER_CLASSES += [
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+REST_FRAMEWORK = {
+    
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication'
-        ],
-        'DEFAULT_RENDERER_CLASSES': [
-            'rest_framework.renderers.JSONRenderer',
-    # 'rest_framework.renderers.BrowsableAPIRenderer',
-        ]
-        }
+    ],
+    'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES
+}
