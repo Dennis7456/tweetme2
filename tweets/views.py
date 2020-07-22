23dfed15ff3fbb8fdfb435cc283f04d91ebd78a1
Dennis7456 <denniskiplangat.dk@gmail.com>
2020-07-22 14:39:53 +0300

    Added CSRF & Client Side Action Buttons

--- tweets/views.py
+++ tweets/views.py
@@ -52,28 +52,28 @@ def tweet_delete_view(request, tweet_id, *args, **kwargs):
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def tweet_action_view(request, tweet_id, *args, **kwargs):
+def tweet_action_view(request, *args, **kwargs):
     # Id is required
     # Action options are: like, unlike and retweet
-
-    serializer = TweetActionSerializer(data=request.POST)
+    serializer = TweetActionSerializer(data=request.data)
     if serializer.is_valid(raise_exception = True):
         data =serializer.validated_data
         tweet_id = data.get("id")
         action = data.get("action")
-
         qs = Tweet.objects.filter(id = tweet_id)
         if not qs.exists():
             return Response({}, status=404)
         obj = qs.first()
         if action == "like":
             obj.likes.add(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
         elif action == "unlike":
             obj.likes.remove(request.user)
         elif action == "retweet":
             #this is todo
             pass
-    return Response({"message":"Tweet removed"}, status=200)
+    return Response({}, status=200)
 
 @api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
